834cda1cc2c4ae021c16e5d150c1d87dc86a361b
Fpsska <fpsska1337@gmail.com>
2021-11-26 11:53:00 +0300

    feat: create SvgTemplate & content__section--left

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Row, Col } from "antd";
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
@@ -12,6 +12,7 @@ import { Modal } from "antd";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { switchAuthStatus } from "../../app/store/authSlice";
+import SvgTemplate from "../Common/SvgTemplate";
 import "./ProfilePage.scss";
 
 const ProfilePage = () => {
@@ -48,9 +49,15 @@ const ProfilePage = () => {
     <Layout
       style={{
         height: "100vh",
+        overflow: "auto",
       }}
     >
-      <Sider trigger={null} collapsible collapsed={collapsed}>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        style={{ position: "relative" }}
+      >
         <div className="logo" />
         <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
           <Menu.Item key="1" icon={<UserOutlined />}>
@@ -62,25 +69,30 @@ const ProfilePage = () => {
           <Menu.Item key="3" icon={<UploadOutlined />}>
             upload
           </Menu.Item>
-          <>
-            <Menu.Item key="4" icon={<LogoutOutlined />} onClick={logOut}>
-              Log Out
-            </Menu.Item>
-            {isModalVisible ? (
-              <>
-                <Modal
-                  title="Exit"
-                  visible={isModalVisible}
-                  onOk={handleConfirm}
-                  onCancel={handleCancel}
-                >
-                  <p>Are you sure?</p>
-                </Modal>
-              </>
-            ) : (
-              <></>
-            )}
-          </>
+        </Menu>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["0"]}
+          style={{ position: "absolute", bottom: "0" }}
+        >
+          <Menu.Item key="1" icon={<LogoutOutlined />} onClick={logOut}>
+            Log Out
+          </Menu.Item>
+          {isModalVisible ? (
+            <>
+              <Modal
+                title="Exit"
+                visible={isModalVisible}
+                onOk={handleConfirm}
+                onCancel={handleCancel}
+              >
+                <p>Are you sure?</p>
+              </Modal>
+            </>
+          ) : (
+            <></>
+          )}
         </Menu>
       </Sider>
       <Layout className="site-layout">
@@ -96,12 +108,197 @@ const ProfilePage = () => {
         <Content
           className="site-layout-background"
           style={{
-            margin: "24px 16px",
-            padding: 24,
+            margin: 0,
+            padding: 0,
             minHeight: 280,
           }}
         >
-          {`Hello, ${userInformation.username}!`}
+          <div className="content">
+            <Row className="content__wrapper">
+              <Col span={4} className="content__section content__section--left">
+                <Row>
+                  <Col style={{ width: "100%" }}>
+                    <div className="content__preview">
+                      <h2 className="content__title">Nomad List</h2>
+                      <span className="content__icon">
+                        <SvgTemplate id="settings" />
+                      </span>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col style={{ width: "100%" }}>
+                    <div className="content__preview content__preview--treads">
+                      <span className="content__icon">
+                        <SvgTemplate id="dialog" />
+                      </span>
+                      <h2 className="content__title content__title--small">
+                        All treads
+                      </h2>
+                    </div>
+                  </Col>
+                </Row>
+                {/*  */}
+                <Row>
+                  <Col style={{ width: "100%" }}>
+                    <div className="content__preview">
+                      <h2 className="content__title">Channels</h2>
+                      <span className="content__counter">11</span>
+                    </div>
+                    <ul className="channels">
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general1
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general2
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general3
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general4
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general5
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general6
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general7
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general8
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general9
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general10
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general11
+                        </a>
+                      </li>
+                      <li className="channels__item">
+                        <a className="channels__link" href="">
+                          # general12
+                        </a>
+                      </li>
+                    </ul>
+                  </Col>
+                </Row>
+                {/*  */}
+                <Row>
+                  <Col style={{ width: "100%" }}>
+                    <div className="content__preview">
+                      <h2 className="content__title">Friends</h2>
+                      <span className="content__counter">82</span>
+                    </div>
+                    <ul className="friends">
+                      <li className="friends__item">
+                        <img
+                          className="friends__image"
+                          src="https://via.placeholder.com/40x40"
+                          alt="img"
+                        />
+                        <span className="friends__name">Orlando Diggs</span>
+                      </li>
+                      <li className="friends__item">
+                        <img
+                          className="friends__image"
+                          src="https://via.placeholder.com/40x40"
+                          alt="img"
+                        />
+                        <span className="friends__name">Carmen Velasco</span>
+                      </li>
+                      <li className="friends__item">
+                        <img
+                          className="friends__image"
+                          src="https://via.placeholder.com/40x40"
+                          alt="img"
+                        />
+                        <span className="friends__name">Marie Jensen</span>
+                      </li>
+                      <li className="friends__item">
+                        <img
+                          className="friends__image"
+                          src="https://via.placeholder.com/40x40"
+                          alt="img"
+                        />
+                        <span className="friends__name">Alex Lee</span>
+                      </li>
+                      <li className="friends__item">
+                        <img
+                          className="friends__image"
+                          src="https://via.placeholder.com/40x40"
+                          alt="img"
+                        />
+                        <span className="friends__name">Leo Gill</span>
+                      </li>
+                      <li className="friends__item">
+                        <img
+                          className="friends__image"
+                          src="https://via.placeholder.com/40x40"
+                          alt="img"
+                        />
+                        <span className="friends__name">Britney Cooper</span>
+                      </li>
+                      <li className="friends__item">
+                        <img
+                          className="friends__image"
+                          src="https://via.placeholder.com/40x40"
+                          alt="img"
+                        />
+                        <span className="friends__name">Orlando Diggs</span>
+                      </li>
+                      <li className="friends__item">
+                        <img
+                          className="friends__image"
+                          src="https://via.placeholder.com/40x40"
+                          alt="img"
+                        />
+                        <span className="friends__name">Carmen Velasco</span>
+                      </li>
+                    </ul>
+                  </Col>
+                </Row>
+              </Col>
+              {/* /. COL LEFT */}
+              <Col
+                span={17}
+                className="content__section content__section--main"
+              ></Col>
+              {/* /. COL MIDDLE */}
+              <Col
+                span={3}
+                className="content__section content__section--right"
+              ></Col>
+            </Row>
+          </div>
+
+          {/* {`Hello, ${userInformation.username}!`} */}
         </Content>
       </Layout>
     </Layout>
