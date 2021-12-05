ceb31af2088f935dda056a4cf549490132048920
Fpsska <fpsska1337@gmail.com>
2021-12-05 13:36:22 +0300

    feat: create warning section

--- src/components/Pages/ChatPages/ChatPageSecond.jsx
+++ src/components/Pages/ChatPages/ChatPageSecond.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 import { Spin } from "antd";
+import { useSelector } from "react-redux";
 
 const ChatPageSecond = () => {
+  const { isFetching } = useSelector((state) => state.ChatReducer);
+
   return (
     <>
-      <Spin
-        size="large"
-        style={{
-          margin:"auto"
-        }}
-      />
+      {isFetching ? (
+        <div className="warning">
+          <img
+            className="warning__image"
+            src={require(`../../../assets/images/warning.png`).default}
+            alt="warning"
+          />
+          <div className="warning__text">
+            <h2 className="warning__title">NSFW Channel</h2>
+            <p className="warning__description">
+              You must be at least eighteen years old to view this channel. Are
+              you over eighteen and willing to see adult content?
+            </p>
+          </div>
+        </div>
+      ) : (
+        <Spin
+          size="large"
+          style={{
+            margin: "auto",
+          }}
+        />
+      )}
     </>
   );
 };
