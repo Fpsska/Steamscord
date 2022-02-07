a90c39ef321636a9680e7974968ffa76bf92b72b
Fpsska <fpsska1337@gmail.com>
2022-02-07 19:09:42 +0300

    refactor: change PORT configuration

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-const express = require("express");
+const express = require('express');
 const axios = require('axios');
 
-const PORT = process.env.PORT
-
-if (PORT == null || PORT == "") {
-    PORT = 8000;
-}
+const PORT = process.env.PORT || 8000
 
 const app = express();
-
-//  Serve the static files from the React app
-app.use(express.static('/public'));
-
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -20,7 +12,7 @@ app.use((req, res, next) => {
     next()
 });
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     axios.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=A15845DCA701187E95FB717C1D208226&steamids=76561198263478170_76561198034948682_76561198084746227_76561197988532919_76561198291514144_76561198873628029_76561198857004420_76561198155088017_76561198075746592")
         .then(response => {
             responseData = response.data.response.players
