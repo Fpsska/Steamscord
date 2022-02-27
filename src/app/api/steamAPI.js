07949a589cb4f10c017e237dfba16ebcf858e280
Fpsska <fpsska1337@gmail.com>
2022-02-28 00:22:27 +0300

    refactor: change rules of height for HTML-elements of home__wrapper, friend, channel, message, warning

--- src/app/api/steamAPI.js
+++ src/app/api/steamAPI.js
@@ -2,10 +2,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const steamAPI = createApi({
     reducerPath: "steamAPI",
-    baseQuery: fetchBaseQuery({ baseUrl: "https://steamscord.herokuapp.com/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://backend-steamscord.herokuapp.com/" }),
     endpoints: (build) => ({
         getProfileInfo: build.query({
-            query: () => "/api"
+            query: () => "/"
         })
     })
 })
