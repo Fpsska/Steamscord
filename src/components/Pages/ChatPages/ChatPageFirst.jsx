6dae225a113870ecbddd1872d455d8cdc97e4377
Fpsska <fpsska1337@gmail.com>
2022-07-23 00:23:10 +0300

    feat: add logic of display Spin ant component in ChatPageFirst.jsx & add ChatHeader, ChatForm components in NoFoundPage.jsx

--- src/components/Pages/ChatPages/ChatPageFirst.jsx
+++ src/components/Pages/ChatPages/ChatPageFirst.jsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import { Spin } from 'antd';
+
+import { switchFetchingStatus } from '../../../app/store/chatSlice';
 
 import CommentsList from '../../Comment/CommentList';
 import ChatHeader from '../../Chat/ChatHeader';
@@ -6,17 +12,40 @@ import ChatForm from '../../Chat/ChatForm';
 
 const ChatPageFirst = ({ availableItems, isError, isLoading }) => {
 
+  const { isFetching } = useSelector(state => state.chatReducer);
+  const { isAuthorized } = useSelector(state => state.authReducer);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    isAuthorized && setTimeout(() => {
+      dispatch(switchFetchingStatus(false));
+    }, 1300);
+    return () => {
+      // clear Interval
+    };
+  }, [isAuthorized]);
+
   return (
     <>
       <ChatHeader
       // enteredSearchValue={enteredSearchValue}
       // setEnteredSearchValue={setEnteredSearchValue}
       />
-      <CommentsList
-        availableItems={availableItems}
-        isLoading={isLoading}
-        isError={isError}
-      />
+      {isFetching ?
+        <Spin
+          size="large"
+          style={{
+            margin: 'auto'
+          }}
+        />
+        :
+        <CommentsList
+          availableItems={availableItems}
+          isLoading={isLoading}
+          isError={isError}
+        />
+      }
       <ChatForm />
     </>
   );
--- src/components/Pages/NoFoundPage/NoFoundPage.jsx
+++ src/components/Pages/NoFoundPage/NoFoundPage.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 import { Empty } from 'antd';
 
+import ChatHeader from '../../Chat/ChatHeader';
+import ChatForm from '../../Chat/ChatForm';
+
 const NoFoundPage = () => {
   return (
     <>
+      <ChatHeader />
       <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} style={{ margin: 'auto' }} />
+      <ChatForm />
     </>
   );
 };
