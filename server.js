db87993a3d220cf8c2711fe90165fb0b070af42b
Fpsska <fpsska1337@gmail.com>
2022-08-01 17:16:12 +0300

    refactor: change logic of handle get request route, change endpoint & change logic of handle app.listen func & update configuration of package/package-lock.json fiels

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
 const express = require('express');
-const axios = require('axios');
-
-const PORT = process.env.PORT || 8000
+const fetch = require('node-fetch');
 
 const app = express();
 
+
+
+// middleware
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT, PATCH, UPDATE");
-    next()
+    next();
 });
 
+
+
+// routes
 app.get('/', (req, res) => {
-    axios.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=A15845DCA701187E95FB717C1D208226&steamids=76561198263478170_76561198034948682_76561198084746227_76561197988532919_76561198291514144_76561198873628029_76561198857004420_76561198155088017_76561198075746592")
-        .then(response => {
-            responseData = response.data.response.players
-            res.json(responseData)
-        })
+    res.send('START route');
+});
+
+app.get('/api/data', async (req, res) => {
+    const response = await fetch("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=A15845DCA701187E95FB717C1D208226&steamids=76561198263478170_76561198034948682_76561198084746227_76561197988532919_76561198291514144_76561198873628029_76561198857004420_76561198155088017_76561198075746592")
+        .then(res => res.json())
+        .catch(err => console.error(err.message || err));
+
+    res.json(response.response);
 });
 
 
-app.listen(PORT, (err) => {
-    if (err) {
-        console.log("LISTEN ERROR: ", err);
-        return;
-    }
-    console.log(`Server started on port ${PORT}`);
-});
+
+// connection
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
